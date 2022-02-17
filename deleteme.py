0dc158807dec82ad3f5ca6b741c45e03c0352622
mycode <green-archer72@users.noreply.github.com>
2022-02-17 21:58:44 +0000

    learning about input function

---
+++ deleteme.py
@@ -0,0 +1,5 @@
+!#/bin/rm
+# This script calls on the executable calld **rm** (remove), which is
+# installed to the */bin* directory. When this script is run,
+# bash will ececute this script with *rm*, which will remove (delete)
+# the script :p
---
+++ py3_shebang.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ell, but that\'s close enough.")
